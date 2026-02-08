dabbd93ee0f22eaabb71a7e004ede9d6820e75f1
Fabio Manganiello <fabio@manganiello.tech>
2026-02-09 00:29:32 +0100

    chore: Don't expose `FileSystemWatcher` in `webmentions.storage.adapters.file`

--- src/python/webmentions/storage/adapters/file/__init__.py
+++ src/python/webmentions/storage/adapters/file/__init__.py
@@ -1,11 +1,9 @@
 from ._adapter import FileSystemMonitor
 from ._model import ContentChange, ContentChangeType
-from ._watcher import FileSystemWatcher
 
 
 __all__ = [
     "ContentChange",
     "ContentChangeType",
-    "FileSystemWatcher",
     "FileSystemMonitor",
 ]

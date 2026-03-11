46bef6365f5d153ed1816a1be422adcc5e29c619
Fabio Manganiello <fabio@manganiello.tech>
2026-03-11 18:33:25 +0100

    Bump version: 0.1.17 → 0.1.18

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## 0.1.18
 
 ### Changed
 - Removed `max-height` from expanded interactions
--- src/python/webmentions/__init__.py
+++ src/python/webmentions/__init__.py
@@ -9,7 +9,7 @@ from ._model import (
     WebmentionType,
 )
 
-__version__ = "0.1.17"
+__version__ = "0.1.18"
 
 __all__ = [
     "ContentTextFormat",
